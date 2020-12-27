b8205e7c28b123516bc38a86cb3e0bdf205c4e7f
choijiyu <jiyou126@naver.com>
2020-12-27 20:51:13 +0900

    1927

---
+++ 최소 힙.py
@@ -0,0 +1,17 @@
+import sys
+import heapq as hq
+
+n = int(sys.stdin.readline())
+h = []
+
+for _ in range(n):
+    number = int(sys.stdin.readline())
+
+    if number == 0:
+        if not h:
+            print(0)
+        else:
+            print(hq.heappop(h))
+
+    elif str(number).isdigit():
+        hq.heappush(h, number)
