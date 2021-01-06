352482fedc68db33a5a4e9201b851016a01bb5c6
choijiyu <jiyou126@naver.com>
2021-01-06 16:28:22 +0900

    BOJ 2309 풀이 추가

--- BOJ/일곱 난쟁이.py
+++ BOJ/일곱 난쟁이.py
@@ -1,3 +1,4 @@
+# index를 저장하는 풀이
 h = [int(input()) for i in range(9)]
 total = sum(h) - 100
 
@@ -13,3 +14,26 @@ h.sort()
 
 for n in h:
     print(n)
+
+
+# flag를 이용한 풀이 (중간에 break하므로 시간 단축)
+p = [int(input()) for _ in range(9)]
+tot = sum(p) - 100
+flag = False
+
+for i in range(8):
+    for j in range(i+1, 9):
+        if p[i] + p[j] == tot:
+            flag = True
+            break
+
+    if flag == True:
+        break
+
+del p[i]
+del p[j-1]
+
+h.sort()
+
+for i in p:
+    print(i)
