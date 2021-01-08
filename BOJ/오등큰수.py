5ef4febc4c6b747b5f296697e26383499e49537a
choijiyu <jiyou126@naver.com>
2021-01-09 00:05:34 +0900

    BOJ 17299 (Update method using Counter class)

--- BOJ/오등큰수.py
+++ BOJ/오등큰수.py
@@ -1,18 +1,16 @@
 import sys
+from collections import Counter
 
 n = int(sys.stdin.readline())
 a = list(map(int, sys.stdin.readline().split()))
 
 stack = []
-ch = [0]*(max(a)+1)
 res = [-1]*n
 
-# 데이터의 갑소가 동일한 인덱스의 데이터 1씩 증가
-for i in a:
-    try:
-        ch[i] += 1
-    except:
-        ch[i] = 1
+# 내장 클래스 Counter를 이용한 더 간결한 방식
+count = Counter(a)
+ch = [count[i] for i in a]
+
 
 for i in range(n-1):
     stack.append(i)
@@ -20,3 +18,17 @@ for i in range(n-1):
         res[stack.pop()] = a[i+1]
 
 print(*res)
+
+
+
+'''
+# check 리스트 선언
+ch = [0]*(max(a)+1)
+
+# 데이터의 값과 동일한 인덱스의 데이터 1씩 증가
+for i in a:
+    try:
+        ch[i] += 1
+    except:
+        ch[i] = 1
+'''
