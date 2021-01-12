a2ab359a2920b94aa3246949dbda44748cb477eb
choijiyu <jiyou126@naver.com>
2021-01-12 21:18:09 +0900

    프로그래머스 heap

---
+++ Programmers/[Heap] 더 맵게.py
@@ -0,0 +1,43 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    h = []
+
+    for i in scoville:
+        heapq.heappush(h, i)
+
+    # 가장 작은 수가 K보다 작으면 수행
+    while h[0] < K:
+        one = heapq.heappop(h)
+        two = heapq.heappop(h) * 2
+        mix = one + two
+        heapq.heappush(h, mix)
+        answer += 1
+
+        # 만약 힙의 길이가 1이고 가장 작은 수가 K보다 작으면
+        # 더이상 스코빌 지수를 K이상으로 만들 수 없으므로
+        if len(h) == 1 and h[0] < K:
+            return -1
+    return answer
+
+
+''' 다른 사람 풀이 (좀 더 깔끔한 풀이!)
+import heapq as hq
+
+def solution(scoville, K):
+
+    hq.heapify(scoville)
+    answer = 0
+    while True:
+        first = hq.heappop(scoville)
+        if first >= K:
+            break
+        if len(scoville) == 0:
+            return -1
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1  
+
+    return answer
+'''
