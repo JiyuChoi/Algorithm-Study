dba3187f78c6a5f9a7da308ec67ced71eaf16a0d
choijiyu <jiyou126@naver.com>
2021-01-23 22:27:38 +0900

    BOJ BackTracking

---
+++ BOJ/[BT] N과 M (4).py
@@ -0,0 +1,15 @@
+def dfs(l, s):
+if l == m:
+    for i in ch:
+        print(i, end=" ")
+    print()
+
+else:
+    for i in range(s, n + 1):
+        ch[l] = i
+        dfs(l + 1, s)
+        s = i + 1
+
+n, m = map(int, input().split())
+ch = [0] * m
+dfs(0, 1)
