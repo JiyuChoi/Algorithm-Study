36075091432cb935acce317b420a8b1d5635df4d
choijiyu <jiyou126@naver.com>
2021-02-09 21:57:14 +0900

    BOJ DFS

--- BOJ/[BFS] 연결 요소의 개수.py
+++ BOJ/[BFS] 연결 요소의 개수.py
@@ -1,10 +1,12 @@
 import sys
+# 파이썬에서는 재귀 허용치가 넘어가면 에러남
+# DFS에서 설정해줘야 런타임 에러가 안남
 sys.setrecursionlimit(10000)
 
 n, m = map(int, sys.stdin.readline().split())
 link = [[0]*(n+1) for _ in range(n+1)]
 cnt = 0
-visited=[0]*(n+1)
+visited = [0]*(n+1)
 
 for _ in range(m):
     x, y = map(int, sys.stdin.readline().split())
