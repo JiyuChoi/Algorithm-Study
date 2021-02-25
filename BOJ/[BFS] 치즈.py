7c2c83ee310b8d32bf7a731e01f3eb7bc533513d
choijiyu <jiyou126@naver.com>
2021-02-25 22:18:56 +0900

    BOJ BFS 2636

---
+++ BOJ/[BFS] 치즈.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+cheese = []
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+
+def bfs(x,y):
+    visited = [[0] * m for _ in range(n)]
+    visited[x][y] = 1
+    d = deque()
+    d.append((x,y))
+    cnt = 0
+    while d:
+        x, y = d.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+                visited[nx][ny] = 1
+                if board[nx][ny]:
+                    board[nx][ny] = 0
+                    cnt += 1
+                else:
+                    d.append((nx,ny))
+    cheese.append(cnt)
+    return cnt
+
+while True:
+    res = bfs(0,0)
+    if not res:
+        break
+
+print(len(cheese)-1)
+print(cheese[-2])
