1e15a2e140744b8d2bfc747f5c46950c62bc603a
choijiyu <jiyou126@naver.com>
2021-02-27 01:05:16 +0900

    BOJ BFS 2638

---
+++ BOJ/[BFS] 치즈 2638.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+cheese = []
+d = deque()
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+def bfs(x,y):
+    # -1로 초기화한 visited 리스트 생성
+    visited = [[-1]*m for _ in range(n)]
+    visited[x][y] = 1
+    d.append((x, y))
+    cnt = 0
+    while d:
+        x, y = d.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 해당 노드가 board 내에 있고 2번이상 방문하지 않은 경우
+            if 0<=nx<n and 0<=ny<m and visited[nx][ny] != 1:
+                # 치즈가 있는 부분(1)이면
+                if board[nx][ny]:
+                    # 방문 횟수 1 증가
+                    visited[nx][ny] += 1
+                    # 만약 1이라면 치즈 녹은 것 표시하고 cnt+1
+                    if visited[nx][ny]:
+                        board[nx][ny] = 0
+                        cnt += 1
+                # 치즈가 없는 부분이라면 방문 처리 후 큐에 추가
+                else:
+                    visited[nx][ny] = 1
+                    d.append((nx, ny))
+    return cnt
+
+# 치즈가 다 녹기 위한 시간
+time = 0
+while True:
+    time += 1
+    res = bfs(0,0)
+    # 만약 치즈가 다 녹았다면 정지
+    if not res:
+        break
+
+print(time-1)
