38b6b92c38ee76dd74861ab9588fcbebf6a0b090
choijiyu <jiyou126@naver.com>
2021-03-08 00:09:09 +0900

    BOJ DFS 14888 (삼성)

--- Programmers/[DFS] 괄호 변환.py
+++ Programmers/[DFS] 괄호 변환.py
@@ -1,5 +1,6 @@
+# 균형잡힌 괄호 문자열의 인덱스 반환
 def balanced_index(p):
-    cnt = 0
+    cnt = 0 # 왼쪽 괄호의 개수
     for i in range(len(p)):
         if p[i] == "(":
             cnt += 1
@@ -8,16 +9,17 @@ def balanced_index(p):
         if cnt == 0:
             return i
 
+# 올바른 문자열인지 판단
 def check_proper(p):
-    cnt = 0
+    cnt = 0 # 왼쪽 괄호의 개수
     for x in p:
         if x == "(":
             cnt += 1
         else:
-            if cnt == 0:
-                return False
             cnt -= 1
-    # 올바른 괄호 문자열
+            if cnt < 0: # 쌍이 맞지 않는 경우 False 반환
+                return False
+    # 올바른 괄호 문자열일 경우 True
     return True
 
 def solution(p):
@@ -27,15 +29,16 @@ def solution(p):
     idx = balanced_index(p)
     u = p[:idx+1]
     v = p[idx+1:]
+    # u가 올바른 문자열일 경우
     if check_proper(u):
         answer = u + solution(v)
+    # u가 올바른 문자열이 아닌 경우
     else:
         answer = "(" + solution(v) + ")"
-        u = list(u[1:-1])
-        for i in range(len(u)):
-            if u[i] == "(":
-                u[i] = ")"
+        u = u[1:-1] # 첫번째 문자와 마지막 문자 제거
+        for x in u:
+            if x == "(":
+                answer += ")"
             else:
-                u[i] = "("
-        answer += "".join(u)
+                answer += "("
     return answer
