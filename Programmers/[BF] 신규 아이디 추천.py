860a93e210f3642b9cb84bc78afb8d3e1d528981
choijiyu <jiyou126@naver.com>
2021-03-16 00:21:36 +0900

    Programmers 카카오 (풀이 수정)

--- Programmers/[BF] 신규 아이디 추천.py
+++ Programmers/[BF] 신규 아이디 추천.py
@@ -1,15 +1,15 @@
-new_id = "abcdefghijklmn.p"
+new_id = "=.="
 
-def changedot(tmp):
-    answer = ''
-    for i in range(len(tmp)-1):
-        if tmp[i] == ".":
-            if tmp[i] != tmp[i + 1]:
-                answer += "."
-        else:
-            answer += tmp[i]
-    answer += tmp[-1]
-    return answer
+# def changedot(tmp):
+#     answer = ''
+#     for i in range(len(tmp)-1):
+#         if tmp[i] == ".":
+#             if tmp[i] != tmp[i + 1]:
+#                 answer += "."
+#         else:
+#             answer += tmp[i]
+#     answer += tmp[-1]
+#     return answer
 
 
 def solution(new_id):
@@ -17,17 +17,23 @@ def solution(new_id):
 
     # 1번
     new_id = new_id.lower()
+
     # 2번
     for x in new_id:
         if x in ["-", "_", "."] or x.islower() or x.isdecimal():
             answer += x
+
     # 3번
-    answer = changedot(answer)
+    # python스러운 풀이!
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    # answer = changedot(answer)
 
     # 4번
     if answer:
         if answer[0] == ".":
             answer = answer[1:]
+
     if answer:
         if answer[-1] == ".":
             answer = answer[:-1]
@@ -41,11 +47,10 @@ def solution(new_id):
         answer = answer[:15]
         if answer[-1] == ".":
             answer = answer[:-1]
+
     # 7번
-    if len(answer) < 3:
-        while len(answer) != 3:
-            answer += answer[-1]
+    # 길이가 3보다 작은 경우 수행
+    while len(answer) < 3:
+        answer += answer[-1]
 
     return answer
-
-print(solution(new_id))
