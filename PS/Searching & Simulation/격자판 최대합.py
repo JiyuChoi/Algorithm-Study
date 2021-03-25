f4e1ae9fd96a49ae7b752162b69cace889144e2a
choijiyu <jiyou126@naver.com>
2021-03-25 17:43:34 +0900

    PS section3 update

--- PS/Searching & Simulation/격자판 최대합.py
+++ PS/Searching & Simulation/격자판 최대합.py
@@ -3,20 +3,21 @@ board = [list(map(int, input().split())) for _ in range(n)]
 res = float("-inf")
 
 for i in range(n):
-    row_sum = col_sum = 0
+    sum1 = sum2 = 0
     for j in range(n):
-        row_sum += board[i][j]
-        col_sum += board[j][i]
-    max_sum = max(row_sum, col_sum)
-    if max_sum > res:
-        res = max_sum
+        sum1 += board[i][j]
+        sum2 += board[j][i]
+    if sum1 > res:
+        res = sum1
+    if sum2 > res:
+        res = sum2
 
-line_sum_1 = line_sum_2 = 0
+sum1 = sum2 = 0
 for i in range(n):
-    line_sum_1 += board[i][i]
-    line_sum_2 += board[i][-(i+1)] # j = n-j-1
-    max_sum = max(line_sum_1, line_sum_2)
-    if max_sum > res:
-        res = max_sum
-
+    sum1 += board[i][i]
+    sum2 += board[i][-(i+1)]
+    if sum1 > res:
+        res = sum1
+    if sum2 > res:
+        res = sum2
 print(res)
