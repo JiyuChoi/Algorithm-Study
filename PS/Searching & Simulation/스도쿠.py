f4c811104366b821217202b13f1c89efbf3852c4
choijiyu <jiyou126@naver.com>
2021-03-28 20:15:03 +0900

    PS section3

---
+++ PS/Searching & Simulation/스도쿠.py
@@ -0,0 +1,24 @@
+def check(board):
+    for i in range(9):
+        row = col = [0] * 10
+        for j in range(9):
+            row[board[i][j]] = 1
+            col[board[j][i]] =1
+        if sum(row) != 9 or sum(col) != 9:
+            return False
+    for i in range(3):
+        for j in range(3):
+            box = [0] * 10
+            for k in range(3):
+                for l in range(3):
+                    box[board[i*3+k][j*3+l]] = 1
+            if sum(box) != 9:
+                return False
+    return True
+
+
+board = [list(map(int, input().split())) for _ in range(9)]
+if check(board):
+    print("YES")
+else:
+    print("NO")
