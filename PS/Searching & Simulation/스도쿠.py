8971505b2653b9400b496cf942a440cc1b25a8f9
choijiyu <jiyou126@naver.com>
2021-03-29 16:54:44 +0900

    PS section3

--- PS/Searching & Simulation/스도쿠.py
+++ PS/Searching & Simulation/스도쿠.py
@@ -2,8 +2,10 @@ def check(board):
     for i in range(9):
         row = col = [0] * 10
         for j in range(9):
+            # row에서 확인
             row[board[i][j]] = 1
-            col[board[j][i]] =1
+            # col에서 확인
+            col[board[j][i]] = 1
         if sum(row) != 9 or sum(col) != 9:
             return False
     for i in range(3):
@@ -11,6 +13,7 @@ def check(board):
             box = [0] * 10
             for k in range(3):
                 for l in range(3):
+                    # 3*3 안에서 확인
                     box[board[i*3+k][j*3+l]] = 1
             if sum(box) != 9:
                 return False
