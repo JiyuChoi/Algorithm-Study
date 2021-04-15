df02175484b1c692ffa4d7568b134c860ef807ca
choijiyu <jiyou126@naver.com>
2021-04-15 22:05:16 +0900

    PS section5

--- PS/Data Structure/교육과정 설계.py
+++ PS/Data Structure/교육과정 설계.py
@@ -1,14 +1,16 @@
-order = list(input())
+from collections import Counter
+
 n = int(input())
+word = [input() for _ in range(2*n-1)]
 
-for i in range(n):
-    class_list = input()
-    stack = []
-    for x in class_list:
-        if x in order:
-            stack.append(x)
+for key, value in Counter(word).items():
+    if value == 1:
+        print(key)
+        break
 
-    if stack == order:
-        print("#%d YES" %(i+1))
-    else:
-        print("#%d NO" %(i+1))
+# Counter의 기능 이용!
+word = [input() for _ in range(n)]
+using = [input() for _ in range(n-1)]
+
+print(*list((Counter(word) - Counter(using)).keys()))
+
