77127c97232ae2ca6024f75d0e89662c9e6019b7
choijiyu <jiyou126@naver.com>
2021-04-18 00:28:30 +0900

    PS section5

---
+++ PS/Data Structure/가장 큰 수.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+n = list(map(int, str(n)))
+
+stack = []
+
+for x in n:
+    while stack and m>0 and stack[-1] < x:
+        stack.pop()
+        m -= 1
+    stack.append(x)
+
+if m != 0:
+    stack = stack[:-m]
+
+print(''.join(map(str, stack)))
