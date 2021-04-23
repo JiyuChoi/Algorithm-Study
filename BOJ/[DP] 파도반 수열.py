fe5e2e4fef4ebb81114b3349ff85749ca73c7cf8
choijiyu <jiyou126@naver.com>
2021-04-24 01:47:41 +0900

    BOJ DP

---
+++ BOJ/[DP] 파도반 수열.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+dy = [0]*101
+dy[1:11] = [1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
+
+# n의 최대가 100이니 dy를 한번에 계산하고 답 출력
+for i in range(11, 101):
+    dy[i] = dy[i-1] + dy[i-5]
+
+for _ in range(t):
+    n = int(input())
+    print(dy[n])
