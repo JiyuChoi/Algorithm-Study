93a02868f4535e06cc2ed3c38c93edf6f8f398d1
choijiyu <jiyou126@naver.com>
2021-05-07 20:48:32 +0900

    PS Section7

--- PS/DFS & BFS/송아지 찾기.py
+++ PS/DFS & BFS/송아지 찾기.py
@@ -16,3 +16,20 @@ while d:
                 ch[nx] = 1
                 d.append(nx)
                 dis[nx] = dis[now] + 1
+
+# 5/7 풀이
+s, e = map(int, input().split())
+dis = [0]*10001
+d = deque()
+d.append(s)
+dis[s] = 1
+
+while d:
+    now = d.popleft()
+    if now == e:
+        print(dis[e]-1)
+        break
+    for i in now+1, now-1, now+5:
+        if 0<=i<=10001:
+            d.append(i)
+            dis[i] = dis[now] + 1
