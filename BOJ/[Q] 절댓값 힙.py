00b5ea030098c3dbd983100fcdb336f638b9158a
choijiyu <jiyou126@naver.com>
2021-05-15 17:00:08 +0900

    BOJ 11286

--- BOJ/[Q] 절댓값 힙.py
+++ BOJ/[Q] 절댓값 힙.py
@@ -9,6 +9,7 @@ for _ in range(n):
     if k != 0:
         hq.heappush(h, (abs(k), k))
     else:
+        # 여기서 try, except 써도 됨!
         if len(h) > 0:
             print(hq.heappop(h)[1])
         else:
