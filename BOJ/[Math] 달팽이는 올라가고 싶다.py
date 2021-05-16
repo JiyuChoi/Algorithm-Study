9e5992a481e787c21d31ce3a05169ddef8f0493d
choijiyu <jiyou126@naver.com>
2021-05-16 23:27:55 +0900

    BOJ 2869

--- BOJ/[Math] 달팽이는 올라가고 싶다.py
+++ BOJ/[Math] 달팽이는 올라가고 싶다.py
@@ -1,3 +1,5 @@
+import math
+
 a, b, v = map(int, input().split())
 
 # 나누어 떨어지는지 구분하지 않기 위해 -1
@@ -6,4 +8,4 @@ print(int(day))
 
 # 나누어 떨어지면 그대로 출력 아니면 +1
 k = (v-b)/(a-b)
-print(int(k) if k == int(k) else int(k)+1)
+print(math.ceil(k))
