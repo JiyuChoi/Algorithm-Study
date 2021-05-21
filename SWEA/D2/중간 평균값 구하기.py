5de24913cb7de8d0a22f7e561132c5ed0c812e48
choijiyu <jiyou126@naver.com>
2021-05-22 00:38:48 +0900

    SWEA 1984 round: 반올림

--- SWEA/D2/중간 평균값 구하기.py
+++ SWEA/D2/중간 평균값 구하기.py
@@ -4,5 +4,5 @@ for i in range(1, T+1):
     number = sorted(list(map(int, input().rstrip().split())))
     number.pop() #O(1)
     number.pop(0) #O(n)
-    # 반올림
-    print(f'#{i} {int(sum(number)/8+0.5)}')
+    # 반올림 round
+    print(f'#{i} {round(sum(number)/8)}')
