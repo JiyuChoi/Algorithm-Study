87b51757c6ff6c0e139abfbe35df61acc4fdf302
choijiyu <jiyou126@naver.com>
2021-05-26 15:15:41 +0900

    SWEA 1983

---
+++ SWEA/D2/초심자의 회문 검사.py
@@ -0,0 +1,10 @@
+T = int(input())
+for t in range(1, T+1):
+    # 문자열 입력받고 오른쪽 공백 지우기
+    word = input().rstrip()
+    # 회문이면 1 출력
+    if word == word[::-1]:
+        print(f'#{t} 1')
+    # 아니면 0 출력
+    else:
+        print(f'#{t} 0')
