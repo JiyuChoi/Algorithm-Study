1ebdf3af6127d7d6cf02df020837a41501346ca8
choijiyu <jiyou126@naver.com>
2021-06-08 21:23:41 +0900

    SWEA 1206

--- SWEA/D3/View.py
+++ SWEA/D3/View.py
@@ -1,1 +1,16 @@
+for t in range(10):
+    n = int(input())
+    height = list(map(int, input().split()))
+    res = 0
 
+    for i in range(2, n-2):
+        max_value = 0
+        for k in (-2, -1, 1, 2):
+            max_value = max(max_value, height[i+k])
+        cha = height[i]-max_value
+        if cha < 0:
+            continue
+        else:
+            res += cha
+
+    print(f'#{t+1} {res}')
