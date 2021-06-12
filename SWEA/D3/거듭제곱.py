f816553d38a844227331655a6c224482c00e1757
choijiyu <jiyou126@naver.com>
2021-06-12 13:13:36 +0900

    SWEA 1217

--- SWEA/D3/거듭제곱.py
+++ SWEA/D3/거듭제곱.py
@@ -1,1 +1,14 @@
+def dfs(l):
+    global tot
+    if l == k:
+        return
+    else:
+        tot *= num
+        dfs(l+1)
 
+for _ in range(10):
+    n = int(input())
+    num, k = map(int, input().split())
+    tot = 1
+    dfs(0)
+    print(f'#{n} {tot}')
