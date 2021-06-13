05a6643a244bd3f154fd5abe41f20c0e5b6f4227
choijiyu <jiyou126@naver.com>
2021-06-13 23:28:31 +0900

    SWEA 2005

--- SWEA/D2/파스칼의 삼각형.py
+++ SWEA/D2/파스칼의 삼각형.py
@@ -1,1 +1,12 @@
-
+for t in range(int(input())):
+    n = int(input())
+    print(f'#{t+1}')
+    print(1)
+    res = [[1]]
+    for i in range(1, n):
+        arr = [1]
+        for j in range(i-1):
+            arr.append(res[i-1][j]+res[i-1][j+1])
+        arr.append(1)
+        res.append(arr)
+        print(*arr)
