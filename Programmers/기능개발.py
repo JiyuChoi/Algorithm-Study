089f1604068c5f78d0d360bdeec662e2dab48c13
choijiyu <jiyou126@naver.com>
2021-06-30 21:08:57 +0900

    프로그래머스 기능개발

--- Programmers/기능개발.py
+++ Programmers/기능개발.py
@@ -1,17 +1,36 @@
+# from math import ceil
+# def solution(progresses, speeds):
+#     answer = []
+#     days = [ceil((100-p)/s) for p, s in zip(progresses, speeds)]
+#     prev = days[0]
+#     cnt = 0
+#     for x in days:
+#         if x <= prev:
+#             cnt += 1
+#         else:
+#             answer.append(cnt)
+#             prev = x
+#             cnt = 1
+#     answer.append(cnt)
+#     return answer
+
 from math import ceil
-def solution(progresses, speeds):
-    answer = []
-    days = [ceil((100-p)/s) for p, s in zip(progresses, speeds)]
+def solution(progress, speeds):
+    days = []
+    res = []
+    for p, s in zip(progress, speeds):
+        day = ceil((100-p)/s)
+        days.append(day)
     prev = days[0]
     cnt = 0
-    for x in days:
-        if x <= prev:
+    for d in days:
+        if prev >= d:
             cnt += 1
         else:
-            answer.append(cnt)
-            prev = x
+            res.append(cnt)
+            prev = d
             cnt = 1
-    answer.append(cnt)
-    return answer
+    res.append(cnt)
+    return res
 
 print(solution([93, 20, 55], [1, 30, 5]))
