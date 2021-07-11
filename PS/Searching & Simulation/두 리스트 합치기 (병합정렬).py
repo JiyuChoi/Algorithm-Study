02ea8c48d309cb3884820c8022e25c20e6e747d7
choijiyu <jiyou126@naver.com>
2021-07-11 16:20:33 +0900

    PS2

--- PS/Searching & Simulation/두 리스트 합치기 (병합정렬).py
+++ PS/Searching & Simulation/두 리스트 합치기 (병합정렬).py
@@ -1,29 +1,52 @@
+# n = int(input())
+# l1 = list(map(int, input().split()))
+# m = int(input())
+# l2 = list(map(int, input().split()))
+#
+# # Merge Sort: O(n) 풀이
+# res = []
+# i = j = 0
+#
+# while True:
+#     if l1[i] < l2[j]:
+#         res.append(l1[i])
+#         i += 1
+#     else:
+#         res.append(l2[j])
+#         j += 1
+#     if i == n-1:
+#         res += l2[j:]
+#         break
+#     if j == m-1:
+#         res += l1[i:]
+#         break
+#
+# print(*res)
+#
+# # O(nlogn) 풀이
+# l1 += l2
+# l1.sort()
+# print(l1)
+
 n = int(input())
-l1 = list(map(int, input().split()))
+nlist = list(map(int, input().split()))
 m = int(input())
-l2 = list(map(int, input().split()))
+mlist = list(map(int, input().split()))
 
-# Merge Sort: O(n) 풀이
 res = []
-i = j = 0
+if n > m:
+    n, m = m, n
+    nlist, mlist = mlist, nlist
 
-while True:
-    if l1[i] < l2[j]:
-        res.append(l1[i])
-        i += 1
-    else:
-        res.append(l2[j])
+i, j = 0, 0
+while i < n and j < m:
+    if nlist[i] > mlist[j]:
+        res.append(mlist[j])
         j += 1
-    if i == n-1:
-        res += l2[j:]
-        break
-    if j == m-1:
-        res += l1[i:]
-        break
+    else:
+        res.append(nlist[i])
+        i += 1
 
-print(*res)
+res += mlist[j:]
 
-# O(nlogn) 풀이
-l1 += l2
-l1.sort()
-print(l1)
+print(res)
