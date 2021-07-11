d2e2d9608580942d039f43653be3f8f069e188a0
choijiyu <jiyou126@naver.com>
2021-07-11 15:12:10 +0900

    PS2

--- PS/Searching & Simulation/숫자만 추출.py
+++ PS/Searching & Simulation/숫자만 추출.py
@@ -1,14 +1,28 @@
-s = input()
-res = ""
+# s = input()
+# res = ""
+#
+# for x in s:
+#     if x.isdecimal():
+#         res += x
+# res = int(res)
+# print(res)
+#
+# cnt = 0
+# for i in range(res):
+#     if not res % (i+1):
+#         cnt += 1
+# print(cnt)
 
-for x in s:
-    if x.isdecimal():
-        res += x
-res = int(res)
-print(res)
+
+string = input()
+num = ''
+for s in string:
+    if s.isdigit():
+        num += s
+print(int(num))
 
 cnt = 0
-for i in range(res):
-    if not res % (i+1):
+for i in range(1, int(num)+1):
+    if int(num) % i == 0:
         cnt += 1
 print(cnt)
