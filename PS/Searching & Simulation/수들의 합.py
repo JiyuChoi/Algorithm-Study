2a955ad44030a1bcd9049574bc9815c7ae21ded4
choijiyu <jiyou126@naver.com>
2021-07-12 23:09:35 +0900

    PS2

--- PS/Searching & Simulation/수들의 합.py
+++ PS/Searching & Simulation/수들의 합.py
@@ -1,38 +1,62 @@
+# n, m = map(int, input().split())
+# num = list(map(int, input().split()))
+# cnt = 0
+# tot = 0
+# i = j = 0
+#
+# while True:
+#     if tot < m:
+#         # 끝점이 n보다 작아야 되는 조건 필요!
+#         if j < n:
+#             tot += num[j]
+#             j += 1
+#         else:
+#             break
+#     elif tot > m:
+#         tot -= num[i]
+#         i += 1
+#     else:
+#         cnt += 1
+#         tot -= num[i]
+#         i += 1
+#
+# print(cnt)
+#
+# # 다른 풀이
+# e = 0
+# # 시작점을 순차적으로 증가
+# for s in range(n):
+#     # 끝점을 가능한만큼 이동시키
+#     while tot < m and e < n:
+#         tot += num[e]
+#         e += 1
+#     # 부분합이 m이면 카운트 증가
+#     if tot == m:
+#         cnt += 1
+#     tot -= num[s]
+#
+# print(cnt)
+
+
+# 7/12 못품!
 n, m = map(int, input().split())
-num = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
+
+i, j = 0, 0
 cnt = 0
 tot = 0
-i = j = 0
-
 while True:
-    if tot < m:
-        # 끝점이 n보다 작아야 되는 조건 필요!
+    if tot == m:
+        tot -= numbers[i]
+        cnt += 1
+        i += 1
+    elif tot > m:
+        tot -= numbers[i]
+        i += 1
+    else:
         if j < n:
-            tot += num[j]
+            tot += numbers[j]
             j += 1
         else:
             break
-    elif tot > m:
-        tot -= num[i]
-        i += 1
-    else:
-        cnt += 1
-        tot -= num[i]
-        i += 1
-
-print(cnt)
-
-# 다른 풀이
-e = 0
-# 시작점을 순차적으로 증가
-for s in range(n):
-    # 끝점을 가능한만큼 이동시키
-    while tot < m and e < n:
-        tot += num[e]
-        e += 1
-    # 부분합이 m이면 카운트 증가
-    if tot == m:
-        cnt += 1
-    tot -= num[s]
-
 print(cnt)
