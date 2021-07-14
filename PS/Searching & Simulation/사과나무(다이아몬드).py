dd3b75a3533022f9c03880e72d172acf996e1c07
choijiyu <jiyou126@naver.com>
2021-07-14 17:48:53 +0900

    PS2

--- PS/Searching & Simulation/사과나무(다이아몬드).py
+++ PS/Searching & Simulation/사과나무(다이아몬드).py
@@ -1,24 +1,37 @@
+# n = int(input())
+# apple = [list(map(int, input().split())) for _ in range(n)]
+# res = 0
+# l = n//2
+#
+# # 투포인터 이용
+# s = e = l
+# for i in range(n):
+#     for j in range(s, e+1):
+#         res += apple[i][j]
+#     if i < l:
+#         s -= 1
+#         e += 1
+#     else:
+#         s += 1
+#         e -= 1
+#
+# print(res)
+#
+#
+# # 인덱스 이용
+# for i in range(n):
+#     if i <= l:
+#         res += sum(apple[i][l-i:l+i+1])
+#     else:
+#         res += sum(apple[i][i-l:l-i])
+#
+# print(res)
+
 n = int(input())
 apple = [list(map(int, input().split())) for _ in range(n)]
 res = 0
 l = n//2
 
-# 투포인터 이용
-s = e = l
-for i in range(n):
-    for j in range(s, e+1):
-        res += apple[i][j]
-    if i < l:
-        s -= 1
-        e += 1
-    else:
-        s += 1
-        e -= 1
-
-print(res)
-
-
-# 인덱스 이용
 for i in range(n):
     if i <= l:
         res += sum(apple[i][l-i:l+i+1])
