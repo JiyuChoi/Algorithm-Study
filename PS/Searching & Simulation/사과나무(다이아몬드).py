232500e201ec0c14fc9a9013a0228579a0d00bb7
choijiyu <jiyou126@naver.com>
2021-07-15 22:39:08 +0900

    PS2

--- PS/Searching & Simulation/사과나무(다이아몬드).py
+++ PS/Searching & Simulation/사과나무(다이아몬드).py
@@ -27,6 +27,7 @@
 #
 # print(res)
 
+
 n = int(input())
 apple = [list(map(int, input().split())) for _ in range(n)]
 res = 0
@@ -37,5 +38,19 @@ for i in range(n):
         res += sum(apple[i][l-i:l+i+1])
     else:
         res += sum(apple[i][i-l:l-i])
+
+print(res)
+
+# 투포인터 방법
+s = e = l
+for i in range(n):
+    for j in range(s, e+1):
+        res += apple[i][j]
+    if i > l:
+        s -= 1
+        e += 1
+    else:
+        s += 1
+        e -= 1
 
 print(res)
