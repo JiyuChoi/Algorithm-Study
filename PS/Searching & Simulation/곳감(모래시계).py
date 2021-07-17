dcf44440a025eee027dc1f68fa6434bc5d86f8e9
choijiyu <jiyou126@naver.com>
2021-07-17 23:22:29 +0900

    PS2

--- PS/Searching & Simulation/곳감(모래시계).py
+++ PS/Searching & Simulation/곳감(모래시계).py
@@ -31,3 +31,22 @@ m = int(input())
 for _ in range(m):
     r, d, cnt = map(int, input().split())
     if d:
+        for _ in range(cnt):
+            board[r-1].append(board[r-1].pop())
+    else:
+        for _ in range(cnt):
+            board[r-1].append(board[r-1].pop(0))
+
+s = 0
+e = n
+l = n//2
+res = 0
+
+for i in range(n):
+    res += sum(board[i][s:e])
+    if i < l:
+        s += 1
+        e -= 1
+    else:
+        s -= 1
+        e += 1
