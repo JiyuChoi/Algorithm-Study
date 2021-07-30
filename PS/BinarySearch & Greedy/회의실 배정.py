60d7b99fde5d813be5184a47e1f42d8bf0dc040a
choijiyu <jiyou126@naver.com>
2021-07-30 23:12:12 +0900

    PS3

--- PS/BinarySearch & Greedy/회의실 배정.py
+++ PS/BinarySearch & Greedy/회의실 배정.py
@@ -1,17 +1,34 @@
-# 회의 수 입력
-n = int(input())
-# 회의 시간 입력 및 종료시간 기준으로 정렬
-time = sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x: (x[1], x[0]))
+# # 회의 수 입력
+# n = int(input())
+# # 회의 시간 입력 및 종료시간 기준으로 정렬
+# time = sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x: (x[1], x[0]))
+#
+# # 첫번째 회의가 끝나는 시간
+# end_time = time[0][1]
+# cnt = 1
+#
+# # 시작:s, 종료:e
+# for s, e in time[1:]:
+#     # 다음회의 시작이 이전회의 종료보다 늦다면 카운트
+#     if s >= end_time:
+#         cnt += 1
+#         end_time = e
+#
+# print(cnt)
 
-# 첫번째 회의가 끝나는 시간
-end_time = time[0][1]
+n = int(input())
+time = []
+for _ in range(n):
+    t1, t2 = map(int, input().split())
+    time.append([t1, t2])
+    time.sort(key=lambda x: (x[1], x[0]))
+
+end_time = time[1][0]
 cnt = 1
 
-# 시작:s, 종료:e
 for s, e in time[1:]:
-    # 다음회의 시작이 이전회의 종료보다 늦다면 카운트
     if s >= end_time:
-        cnt += 1
         end_time = e
+        cnt += 1
 
 print(cnt)
