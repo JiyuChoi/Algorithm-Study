b25535418b92c07d0b85191b3dc6a5ad238af472
choijiyu <jiyou126@naver.com>
2021-08-02 23:48:05 +0900

    PS3

--- PS/BinarySearch & Greedy/씨름선수.py
+++ PS/BinarySearch & Greedy/씨름선수.py
@@ -13,7 +13,7 @@
 # print(cnt)
 
 n = int(input())
-people = sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x: -x[0])
+people = sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x: (-x[0], -x[1]))
 
 cnt = 1
 target = people[0][1]
