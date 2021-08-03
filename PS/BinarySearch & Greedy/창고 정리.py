507aa1c2e412f4b07eda10e37cf9e977d315d45b
choijiyu <jiyou126@naver.com>
2021-08-03 23:25:12 +0900

    PS3

--- PS/BinarySearch & Greedy/창고 정리.py
+++ PS/BinarySearch & Greedy/창고 정리.py
@@ -1,12 +1,23 @@
+# l = int(input())
+# box = sorted(list(map(int, input().split())))
+# m = int(input())
+#
+# # m번 박스 이동 수행
+# for _ in range(m):
+#     box[-1] -= 1
+#     box[0] += 1
+#     box.sort()
+#
+# # 최대 높이 - 최소 높이
+# print(box[-1]-box[0])
+
 l = int(input())
-box = sorted(list(map(int, input().split())))
+h = sorted(list(map(int, input().split())))
 m = int(input())
 
-# m번 박스 이동 수행
 for _ in range(m):
-    box[-1] -= 1
-    box[0] += 1
-    box.sort()
+    h[-1] -= 1
+    h[0] += 1
+    h.sort()
 
-# 최대 높이 - 최소 높이
-print(box[-1]-box[0])
+print(h[-1]-h[0])
