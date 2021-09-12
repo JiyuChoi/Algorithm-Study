83421719d8c0c551dda3848456262ef631efd75a
choijiyu <jiyou126@naver.com>
2021-09-12 20:23:20 +0900

    PS4

--- PS/BinarySearch & Greedy/침몰하는 타이타닉.py
+++ PS/BinarySearch & Greedy/침몰하는 타이타닉.py
@@ -17,6 +17,19 @@ while people:
 
 print(cnt)
 
-
+# 9/11
 n, m = map(int, input().split())
-people = sorted(list(map(int, input().split())))
+p = sorted(list(map(int, input().split())))
+cnt = 0
+
+s, e = 0, n-1
+while s <= e:
+    if p[s] + p[e] > m:
+        e -= 1
+        cnt += 1
+    else:
+        s += 1
+        e -= 1
+        cnt += 1
+
+print(cnt)
