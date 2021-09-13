e18ba25fde6c025536d1005b2cda1b4cdefed81a
choijiyu <jiyou126@naver.com>
2021-09-13 21:20:50 +0900

    BOJ 1654

---
+++ BOJ/[BS] 랜선 자르기.py
@@ -0,0 +1,17 @@
+import sys
+k, n = map(int, sys.stdin.readline().split())
+line = [int(sys.stdin.readline()) for _ in range(k)]
+s, e = 1, max(line) #s이 0이면 ZeroDivisionError 발생
+res = 0
+
+while s <= e:
+    mid = (s+e)//2
+    cnt = 0
+    for l in line:
+        cnt += l//mid
+    if cnt < n:
+        e = mid - 1
+    else:
+        s = mid + 1
+        res = mid
+print(res)
