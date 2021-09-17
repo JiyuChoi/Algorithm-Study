9c770f10beae6ffc3aee42c8a4621ff27dc435f4
choijiyu <jiyou126@naver.com>
2021-09-17 22:10:21 +0900

    BOJ 9012

---
+++ BOJ/[Stack] 괄호.py
@@ -0,0 +1,38 @@
+T = int(input())
+for _ in range(T):
+    cnt = 0
+    ps = input()
+    for j in ps:
+        if j == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        #닫는 개수가 더 많거나 VPS가 아닌 경우
+        if cnt < 0:
+            print("NO")
+            break
+    # 여는 개수와 닫는 개수가 같은 경우
+    if cnt == 0:
+        print("YES")
+    # 여는 개수가 더 많은 경우
+    elif cnt > 0:
+        print("NO")
+
+# 9/17
+n = int(input())
+for _ in range(n):
+    strings = input()
+    stack = []
+    for char in strings:
+        if char == "(":
+            stack.append(char)
+        elif char == ")":
+            if len(stack) != 0 and stack[-1] == "(":
+                stack.pop()
+            else:
+                stack.append(")")
+                break
+    if len(stack) == 0:
+        print("YES")
+    else:
+        print("NO")
