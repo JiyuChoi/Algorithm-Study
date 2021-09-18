457d0dd1219508a22f867fb5adea16d5ffcde22a
choijiyu <jiyou126@naver.com>
2021-09-18 21:54:40 +0900

    BOJ 1158

--- BOJ/[Q] 요세푸스 문제.py
+++ BOJ/[Q] 요세푸스 문제.py
@@ -5,6 +5,7 @@ d = deque([i+1 for i in range(n)])
 res = []
 cnt = 1
 
+# deque 이용 방법 (시간이 오래걸림)
 while d:
     p = d.popleft()
     if cnt == k:
@@ -13,5 +14,13 @@ while d:
     else:
         cnt += 1
         d.append(p)
+
+# 제거할 수의 인덱스를 이용한 방법
+num = 0
+for _ in range(n):
+    num += k-1
+    if num >= len(d):
+        num %= len(d)%num
+    res.append(str(d.pop(num)))
 
 print("<", ", ".join(res), ">", sep="")
