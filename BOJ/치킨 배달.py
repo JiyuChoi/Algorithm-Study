d76ee96cbdd4c0638f750bd5d4674c14dbb2c522
choijiyu <jiyou126@naver.com>
2021-09-21 14:47:49 +0900

    BOJ 15686

--- BOJ/치킨 배달.py
+++ BOJ/치킨 배달.py
@@ -49,3 +49,36 @@ ch = [0]*M
 dfs(0, 0)
 
 print(min_v)
+
+# 9/21 풀이
+def dfs(s, l):
+    global res
+    if l == m:
+        tot_dis = 0
+        for x, y in home:
+            dis = float("inf")
+            for i, j in ch:
+                dis = min(dis, abs(x-i)+abs(y-j))
+            tot_dis += dis
+        if res > tot_dis:
+            res = tot_dis
+    else:
+        for i in range(s, len(chicken)):
+            ch[l] = chicken[i]
+            s = i+1
+            dfs(s, l+1)
+
+
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+home, chicken = [], []
+ch = [0]*m
+res = float("inf")
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            home.append((i,j))
+        elif board[i][j] == 2:
+            chicken.append((i,j))
+dfs(0, 0)
+print(res)
