4b8c7e1110f296d3b8e514c17c810d1d08318771
choijiyu <jiyou126@naver.com>
2021-09-26 16:08:09 +0900

    BOJ 6588/9020 (골드바흐의 추측)

--- BOJ/[Math] 골드바흐의 추측.py
+++ BOJ/[Math] 골드바흐의 추측.py
@@ -1,3 +1,4 @@
+# 6588
 import sys
 r = 1000000
 ch = [0]*(r+1)
@@ -13,9 +14,25 @@ while True:
         break
 
     for i in range(3, r):
-        if ch[i] == 0:
-            if ch[n-i] == 0 and n-i > 2:
+        if ch[i] == 0 and ch[n-i] == 0:
                 print(f'{n} = {i} + {n-i}')
                 break
     else:
         print("Goldbach's conjecture is wrong.")
+
+
+# 9020
+r = 10000
+ch = [0]*(r+1)
+for i in range(2, int(r**0.5)+1):
+    if ch[i] == 0:
+        for j in range(i*2, r+1, i):
+            ch[j] = 1
+
+for _ in range(int(input())):
+    n = int(input())
+    m = n//2
+    for i in range(m, 1, -1):
+        if ch[i] == 0 and ch[n-i] == 0:
+            print(i, n-i)
+            break
