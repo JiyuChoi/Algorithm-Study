2e6e773dd04e71c24ba165522ff5e7e41cd78c64
choijiyu <jiyou126@naver.com>
2021-09-27 11:23:22 +0900

    BOJ 1260

--- BOJ/[DFS, BFS] DFS와 BFS.py
+++ BOJ/[DFS, BFS] DFS와 BFS.py
@@ -35,3 +35,35 @@ def bfs(l):
 dfs(v)
 print()
 bfs(v)
+
+
+# 9/27
+n, m, v = map(int, input().split())
+route = [[0]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    route[a][b] = route[b][a] = 1
+
+visited = [0]*(n+1)
+def dfs(l):
+    visited[l] = 1
+    print(l, end=" ")
+    for i in range(1, n+1):
+        if not visited[i] and route[l][i]:
+            dfs(i)
+
+d = deque()
+def bfs(l):
+    d.append(l)
+    visited[l] = 0
+    while d:
+        l = d.popleft()
+        print(l, end=" ")
+        for i in range(1, n+1):
+            if visited[i] and route[l][i]:
+                d.append(i)
+                visited[i] = 0
+
+dfs(v)
+print()
+bfs(v)
