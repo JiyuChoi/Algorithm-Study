ab0b7e48205e41d88b18f1ade2d59e788354670d
choijiyu <jiyou126@naver.com>
2021-09-27 13:03:25 +0900

    BOJ 2667 (BFS 풀이)

--- BOJ/[BFS] 단지번호붙이기.py
+++ BOJ/[BFS] 단지번호붙이기.py
@@ -59,3 +59,35 @@ print(len(res))
 res.sort()
 for i in res:
     print(i)
+
+# bfs
+from collections import deque
+
+n = int(input())
+board = [list(map(int, input())) for _ in range(n)]
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+d = deque()
+res = []
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            d.append((i, j))
+            board[i][j] = 0
+            cnt = 0
+            while d:
+                cnt += 1
+                x, y = d.popleft()
+                for k in range(4):
+                    nx = x + dx[k]
+                    ny = y + dy[k]
+                    if 0<=nx<n and 0<=ny<n and board[nx][ny]:
+                        board[nx][ny] = 0
+                        d.append((nx, ny))
+            res.append(cnt)
+
+print(len(res))
+for x in sorted(res):
+    print(x)
