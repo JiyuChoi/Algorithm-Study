71131b080cfea1b7bc1d91436cecfe8504720dd0
choijiyu <jiyou126@naver.com>
2021-09-28 10:07:14 +0900

    BOJ 1697

--- BOJ/[BFS] 숨바꼭질.py
+++ BOJ/[BFS] 숨바꼭질.py
@@ -1,23 +1,42 @@
+# from collections import deque
+#
+# # 위치 입력 받기
+# n, k = map(int, input().split())
+# max_v = 100001
+# dis = [0]*max_v
+#
+# d = deque()
+# d.append(n)
+#
+# while d:
+#     now = d.popleft()
+#     # 현재 위치가 도착지점과 같은 경우 break
+#     if now == k:
+#         print(dis[now])
+#         break
+#
+#     # -1, +1, *2의 경우를 나누어 계산
+#     for next in (now-1, now+1, 2*now):
+#         # 범위 내에 있고, 중복되지 않은 경우
+#         if 0 <= next < max_v and dis[next] == 0:
+#             d.append(next)
+#             dis[next] = dis[now] + 1
+
+
 from collections import deque
 
-# 위치 입력 받기
 n, k = map(int, input().split())
+
 max_v = 100001
 dis = [0]*max_v
-
-d = deque()
-d.append(n)
+d = deque([n])
 
 while d:
     now = d.popleft()
-    # 현재 위치가 도착지점과 같은 경우 break
     if now == k:
-        print(dis[now])
+        print(dis[k])
         break
-
-    # -1, +1, *2의 경우를 나누어 계산
-    for next in (now-1, now+1, 2*now):
-        # 범위 내에 있고, 중복되지 않은 경우
-        if 0 <= next < max_v and dis[next] == 0:
-            d.append(next)
-            dis[next] = dis[now] + 1
+    for x in (now+1, now-1, now*2):
+        if 0 <= x < max_v and dis[x] == 0:
+            d.append(x)
+            dis[x] = dis[now] + 1
