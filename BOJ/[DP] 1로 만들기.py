d5f195e662e81a0b7ab4ba29c2993a5d0f23c883
choijiyu <jiyou126@naver.com>
2021-09-29 11:57:31 +0900

    BOJ 1463

--- BOJ/[DP] 1로 만들기.py
+++ BOJ/[DP] 1로 만들기.py
@@ -5,7 +5,7 @@ for i in range(4, n+1):
     dy.append(dy[i-1] + 1)
     if i % 2 == 0:
         dy[i] = min(dy[i], dy[i//2]+1)
-    elif i % 3 == 0:
+    if i % 3 == 0: #elif가 아닌 if로 가야함 6의 경우 3으로 나눈 값이 더 작음
         dy[i] = min(dy[i], dy[i//3]+1)
 
-print(dy[-1])
+print(dy[n])
