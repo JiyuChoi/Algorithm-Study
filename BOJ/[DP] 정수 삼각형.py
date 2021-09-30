234b4f9508a71cf0d4dd0a49eb032c0355db7f5f
choijiyu <jiyou126@naver.com>
2021-09-30 13:31:54 +0900

    BOJ 1932

--- BOJ/[DP] 정수 삼각형.py
+++ BOJ/[DP] 정수 삼각형.py
@@ -13,3 +13,21 @@ for i in range(1, n):
             dy[i][j] = max(dy[i-1][j-1], dy[i-1][j]) + tri[i][j]
 
 print(max(dy[n-1]))
+
+# 9/30
+n = int(input())
+tri = [list(map(int, input().split())) for _ in range(n)]
+dy = [[] for _ in range(n)]
+dy[0].append(tri[0][0])
+
+for i in range(1, n):
+    l = len(tri[i])
+    for j in range(l):
+        if j == 0:
+            dy[i].append(dy[i-1][j] + tri[i][j])
+        elif j == l - 1:
+            dy[i].append(dy[i-1][-1] + tri[i][j])
+        else:
+            dy[i].append(max(dy[i-1][j-1], dy[i-1][j]) + tri[i][j])
+
+print(max(dy[-1]))
