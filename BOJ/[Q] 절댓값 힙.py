cd33c5b2a07b37189a2121c4b4cd46c93349cd80
choijiyu <jiyou126@naver.com>
2021-10-15 14:47:54 +0900

    BOJ 11286

--- BOJ/[Q] 절댓값 힙.py
+++ BOJ/[Q] 절댓값 힙.py
@@ -1,3 +1,20 @@
+# import heapq as hq
+# import sys
+#
+# n = int(sys.stdin.readline())
+# h = []
+#
+# for _ in range(n):
+#     k = int(sys.stdin.readline())
+#     if k != 0:
+#         hq.heappush(h, (abs(k), k))
+#     else:
+#         # 여기서 try, except 써도 됨!
+#         if len(h) > 0:
+#             print(hq.heappop(h)[1])
+#         else:
+#             print(0)
+
 import heapq as hq
 import sys
 
@@ -5,12 +22,9 @@ n = int(sys.stdin.readline())
 h = []
 
 for _ in range(n):
-    k = int(sys.stdin.readline())
-    if k != 0:
-        hq.heappush(h, (abs(k), k))
+    num = int(sys.stdin.readline())
+    if num == 0:
+        print(hq.heappop(h)[1] if h else 0)
     else:
-        # 여기서 try, except 써도 됨!
-        if len(h) > 0:
-            print(hq.heappop(h)[1])
-        else:
-            print(0)
+        hq.heappush(h, (abs(num), num))
+        #Q에 push할 때, 첫번째 값 기준 정렬 -> 두번째 값 기준 정렬로 들어감
