ae2e4d9b5a140c1c8431df9653cc95c09ca24784
choijiyu <jiyou126@naver.com>
2021-10-19 22:33:52 +0900

    Programmers Sort

---
+++ Programmers/[Sort] K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    for s, e, idx in commands:
+        answer.append(list(sorted(array[s-1:e]))[idx-1])
+    return answer
+
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(array, commands))
