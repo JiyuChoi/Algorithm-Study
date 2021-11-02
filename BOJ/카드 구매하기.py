35273d6853af98c9af75ba4f7de638a7ae122e25
choijiyu <jiyou126@naver.com>
2021-11-02 21:44:58 +0900

    BOJ 11052

--- BOJ/카드 구매하기.py
+++ BOJ/카드 구매하기.py
@@ -1,24 +1,12 @@
 import sys
 
-def dfs(l):
-    global max_value
-    if sum(card) == n:
-        res = sum([cards[x-1] for x in card if x != 0])
-        if res > max_value:
-            max_value = res
-        return
-    if l == n or sum(card) > n:
-        return
-    else:
-        for i in range(1, n+1):
-            if card[l] == 0:
-                card[l] = i
-                dfs(l+1)
-                card[l] = 0
-
-max_value = 0
 n = int(input())
-cards = list(map(int, sys.stdin.readline().split()))
-card = [0]*n
-dfs(0)
-print(max_value)
+cards = [0] + list(map(int, sys.stdin.readline().split()))
+dp = [0]*(n+1)
+
+for i in range(1, n+1):
+    for k in range(1, i+1):
+        dp[i] = max(dp[i], cards[k] + dp[i-k])
+
+print(dp[i])
+
