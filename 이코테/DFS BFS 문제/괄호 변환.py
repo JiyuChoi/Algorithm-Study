cb23bc75e9d727df6f809e1fa37ae4e27a7ff4ef
choijiyu <jiyou126@naver.com>
2021-11-05 13:33:25 +0900

    PS 19

--- 이코테/DFS BFS 문제/괄호 변환.py
+++ 이코테/DFS BFS 문제/괄호 변환.py
@@ -1,1 +1,39 @@
-w = input()
+def divided(w):
+    cnt = 0
+    for i in range(len(w)):
+        if w[i] == "(":
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == 0:
+            return w[:i+1], w[i+1:]
+
+def isRight(u):
+    cnt = 0
+    for i in range(len(u)):
+        if u[i] == "(":
+            cnt += 1
+        else:
+            cnt -= 1
+        if u[i] == ")" and cnt < 0:
+            return False
+    return True
+
+def solution(w):
+    if w == "":
+        return w
+
+    u, v = divided(w)
+
+    if isRight(u):
+        res = u + solution(v)
+    else:
+        res = "(" + solution(v) + ")"
+        for p in u[1:len(u) - 1]:
+            if p == '(':
+                res += ')'
+            else:
+                res += '('
+    return res
+
+print(solution(input()))
