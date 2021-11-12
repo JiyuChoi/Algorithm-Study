0af3aa0d2f6ce4f4a99331203a99567f87785335
choijiyu <jiyou126@naver.com>
2021-11-12 09:53:27 +0900

    PS 21

--- 이코테/DFS BFS 문제/인구 이동.py
+++ 이코테/DFS BFS 문제/인구 이동.py
@@ -1,46 +1,45 @@
 from collections import deque
+import sys
 
-N, L, R = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
+N, L, R = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-def bfs(x, y, index):
+def bfs(x, y):
     united = []
     united.append((x, y))
     q = deque()
     q.append((x, y))
-    union[x][y] = index
-    summary = graph[x][y]
-    count = 1
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and union[nx][ny] == -1:
+            if 0 <= nx < N and 0 <= ny < N and not visit[nx][ny]:
                 if L <= abs(graph[nx][ny] - graph[x][y]) <= R:
+                    visit[nx][ny] = 1
                     q.append((nx, ny))
-                    union[nx][ny] = index
-                    summary += graph[nx][ny]
-                    count += 1
                     united.append((nx, ny))
-    for i, j in united:
-        graph[i][j] = summary // count
+    return united
 
 res = 0
-
 while True:
-    idx = 0
-    union = [[-1]*N for _ in range(N)]
+    visit = [[0]*N for _ in range(N)]
+    is_move = False
     for i in range(N):
         for j in range(N):
-            if union[i][j] == -1:
-                bfs(i, j, idx)
-                idx += 1
-    if idx == N*N:
+            if visit[i][j] == 0:
+                visit[i][j] = 1
+                united = bfs(i, j)
+                if len(united) > 1:
+                    is_move = True
+                    num = sum([graph[x][y] for x, y in united]) // len(united)
+                    for x, y in united:
+                        graph[x][y] = num
+
+    if not is_move:
         break
     res += 1
-
 print(res)
