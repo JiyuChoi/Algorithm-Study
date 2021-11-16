1d8b502c250ca80e91705dd914e56053a0ee852c
choijiyu <jiyou126@naver.com>
2021-11-16 22:20:41 +0900

    BOJ 17144 Update

--- BOJ/미세먼지 안녕!.py
+++ BOJ/미세먼지 안녕!.py
@@ -1,47 +1,40 @@
-from collections import deque
-import copy
-
-def bfs():
-    while mess:
-        x, y, s = mess.popleft()
-        if s == t:
-            return
-        spread = a[x][y]//5
-        cnt = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < r and 0 <= ny < c and a[nx][ny] != -1:
-                mess.append((nx, ny, s+1))
-                tmp[nx][ny] += spread
-                cnt += 1
-        tmp[x][y] += a[x][y] - (spread * cnt)
-
-        mess.append((x, y, s+1))
-    for i in range(r):
-        for j in range(c):
-            a[i][j] += tmp[i][j]
-
 r, c, t = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(r)]
-
-mess = deque()
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-tmp = [[0]*c for _ in range(r)]
-for i in range(r):
-    for j in range(c):
-        if a[i][j] == -1:
-            tmp[i][j] = -1
-        elif a[i][j] != 0: # 한 번 돈 것을 다시 mess에 추가하는 로직으로 수정
-            mess.append((i, j, 0))
+def spread():
+    tmp = [[0] * c for _ in range(r)]
+    for x in range(r):
+        for y in range(c):
+            if a[x][y] >= 5:
+                mess = a[x][y] // 5
+                cnt = 0
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if 0 <= nx < r and 0 <= ny < c and a[nx][ny] != -1:
+                        tmp[nx][ny] += mess
+                        cnt += 1
+                # 나눠준 것만큼 뺀다
+                a[x][y] = a[x][y] - mess * cnt
+
+    for i in range(r):
+        for j in range(c):
+            # 추후에 받은 양 더하기
+            a[i][j] += tmp[i][j]
+
+def cleaner():
+    for i in range(r):
+        if a[i][0] == -1 and a[i+1][0] == -1:
+            aircleaner = (i, i+1)
+            break
+
 
 for _ in range(t):
-    bfs()
-    for x in tmp:
+    cleaner()
+    spread()
+    for x in a:
         print(x)
-    print()
-
 
