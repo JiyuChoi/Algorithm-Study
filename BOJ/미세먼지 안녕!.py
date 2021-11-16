dbd45171b7cc05f5b38b5c2e6a1f9f8db47e4b42
choijiyu <jiyou126@naver.com>
2021-11-16 23:50:29 +0900

    BOJ 17144 Update

--- BOJ/미세먼지 안녕!.py
+++ BOJ/미세먼지 안녕!.py
@@ -1,8 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 r, c, t = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(r)]
+a = [list(map(int, input().saplit())) for _ in range(r)]
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
+
+for i in range(r):
+    if a[i][0] == -1 and a[i+1][0] == -1:
+        ac = (i, i+1)
+        break
 
 def spread():
     tmp = [[0] * c for _ in range(r)]
@@ -25,16 +33,54 @@ def spread():
             # 추후에 받은 양 더하기
             a[i][j] += tmp[i][j]
 
-def cleaner():
-    for i in range(r):
-        if a[i][0] == -1 and a[i+1][0] == -1:
-            aircleaner = (i, i+1)
-            break
+def upperbound():
+    temp = a[ac[0]][c-1]
+    for i in range(c-2, 0, -1):
+        a[ac[0]][i+1] = a[ac[0]][i]
+
+    temp2 = a[0][c-1]
+    for i in range(ac[0]-1):
+        a[i][c-1] = a[i+1][c-1]
+    a[ac[0]-1][c-1] = temp
+
+    temp3 = a[0][0]
+    for i in range(c-1):
+        a[0][i] = a[0][i+1]
+    a[0][c-2] = temp2
+
+    for i in range(ac[0]-1, 1, -1):
+        a[i][0] = a[i-1][0]
+    a[ac[0]][1] = 0
+    a[1][0] = temp3
+
+def lowerbound():
+    temp = a[ac[1]][c-1]
+    for i in range(c-2, 0, -1):
+        a[ac[1]][i+1] = a[ac[1]][i]
+
+    temp2 = a[r-1][c-1]
+    for i in range(r-1, ac[1], -1):
+        a[i][c-1] = a[i-1][c-1]
+    a[ac[1]+1][c-1] = temp
+
+    temp3 = a[r-1][0]
+    for i in range(c-2):
+        a[r-1][i] = a[r-1][i+1]
+    a[r-1][c-2] = temp2
+
+    for i in range(ac[1]+1, r-1):
+        a[i][0] = a[i+1][0]
+    a[ac[1]][1] = 0
+    a[r-2][0] = temp3
 
 
 for _ in range(t):
-    cleaner()
     spread()
-    for x in a:
-        print(x)
+    upperbound()
+    lowerbound()
 
+res = 0
+for row in a:
+    res += sum(row)
+
+print(res+2)
