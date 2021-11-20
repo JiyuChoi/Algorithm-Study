3b67e10a0078b5200738f7cd56ef9ab299c1e4f6
choijiyu <jiyou126@naver.com>
2021-11-20 23:47:04 +0900

    2020 Kakao blind recruitment

--- 이코테/구현 문제/기둥과 보 설치.py
+++ 이코테/구현 문제/기둥과 보 설치.py
@@ -1,9 +1,36 @@
+def check(answer):
+    # 구조물을 전부 검사하는 방식
+    for x, y, a, in answer:
+        if a == 0:
+            if y == 0 or (x-1, y, 1) in answer or (x, y-1, 0) in answer or (x, y, 1) in answer:
+                continue
+            else:
+                return False
+        elif a == 1:
+            if (x, y-1, 0) in answer or (x+1, y-1, 0) in answer or ((x-1, y, 1) in answer and (x+1, y, 1) in answer):
+                continue
+            else:
+                return False
+    return True
+
 def solution(n, bulid_frame):
-    answer = []
+    answer = set()
+    for frame in bulid_frame:
+        x, y, a, b = frame
+        if b == 1:
+            # 구조물을 먼저 설치하고 맞지 않는다면 제거
+            answer.add((x, y, a))
+            if not check(answer):
+                answer.remove((x, y, a))
+        elif b == 0:
+            # 구조물을 먼저 제거하고 맞지 않다면 추가
+            answer.remove((x, y, a))
+            if not check(answer):
+                answer.add((x, y, a))
 
-    return answer
-
+    answer = map(list, answer)
+    return sorted(answer)
 
 n = 5
-build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
-solution(n, build_frame)
+build_frame = [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
+print(solution(n, build_frame))
