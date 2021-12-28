bee628656b206a5d8be6384c070f18a606263f86
JiyuChoi <jiyou126@naver.com>
2021-12-28 23:08:14 +0900

    Programmers Kakao n진수 게임

---
+++ Programmers/n진수 게임.py
@@ -0,0 +1,21 @@
+def convert(i, n):
+    tmp = "0123456789ABCDEF"
+
+    q, r = divmod(i, n) // 몫과 나머지 튜플로 주어짐
+
+    if q == 0:
+        return tmp[r]
+    else:
+        return convert(q, n) + tmp[r]
+
+
+def solution(n, t, m, p):
+    answer = ''
+    res = ''
+    for x in range(t * m):
+        res += convert(x, n)
+
+    for i in range(p - 1, t * m, m):
+        answer += res[i]
+
+    return answer
