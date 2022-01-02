af9674e3513a9ba06ef1ef91184b08c0bdb67e5d
JiyuChoi <jiyou126@naver.com>
2022-01-02 15:12:19 +0900

    BOJ 1992

--- BOJ/쿼드트리.py
+++ BOJ/쿼드트리.py
@@ -2,7 +2,7 @@ def quad_tree(x, y, n):
     color = arr[x][y]
     for i in range(x, x+n):
         for j in range(y, y+n):
-            if arr[i][j] != color:
+            if arr[i][j] != color: # 범위안에 한개라도 다른경우는 4분면으로 나눠서 다시 검색
                 answer.append("(")
                 quad_tree(x, y, n//2)
                 quad_tree(x, y + n // 2, n // 2)
