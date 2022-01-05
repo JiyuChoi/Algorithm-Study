df597549c1033bd87bda5714a9d69b34da7677f9
JiyuChoi <jiyou126@naver.com>
2022-01-05 12:58:20 +0900

    Programmers Kakao 캐시

--- Programmers/캐시.py
+++ Programmers/캐시.py
@@ -1,18 +1,20 @@
+from collections import deque
+
 def solution(cacheSize, cities):
     answer = 0
-    cache = []
+    cache = deque([])
     for city in cities:
         city = city.lower()
         if cacheSize:
             if city in cache:
-                cache.pop(cache.index(city))
-                cache.append(city)
+                cache.remove(city)
                 answer += 1
             else:
                 if len(cache) == cacheSize:
-                    cache.pop(0)
-                cache.append(city)
+                    cache.popleft()
                 answer += 5
+            cache.append(city)
         else:
             answer += 5
+
     return answer
