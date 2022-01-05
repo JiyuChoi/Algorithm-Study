80292a927f39cf6ed591ae9f4d102fc4c8530a2b
JiyuChoi <jiyou126@naver.com>
2022-01-05 10:22:33 +0900

    Programmers Naver 상호평가

--- Programmers/상호 평가.py
+++ Programmers/상호 평가.py
@@ -1,10 +1,16 @@
 def solution(scores):
     answer = ''
-    for idx, score in enumerate(list(map(list, (zip(*scores))))):
-        if score[idx] in (max(score), min(score)) and score.count(score[idx]) == 1:
-            del score[idx]
+    for i in range(len(scores)):
+        s = []
+        for j in range(len(scores)):
+            s.append(scores[j][i])
 
-        avg = sum(score)/len(score)
+        if s[i] == min(s) and s.count(s[i]) == 1:
+            del s[i]
+        elif s[i] == max(s) and s.count(s[i]) == 1:
+            del s[i]
+
+        avg = sum(s)/len(s)
 
         if avg >= 90:
             answer += 'A'
@@ -20,3 +26,8 @@ def solution(scores):
     return answer
 
 print(solution([[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]]))
+
+# python 이용!
+# for idx, score in enumerate(list(map(list, (zip(*scores))))):
+    #     if score[idx] in (max(score), min(score)) and score.count(score[idx]) == 1:
+    #         del score[idx]
