e8b6a653613cac0caa98b4871d488157055a42dc
JiyuChoi <jiyou126@naver.com>
2022-01-06 20:28:09 +0900

    Programmers 파일명 정렬

--- Programmers/파일명 정렬.py
+++ Programmers/파일명 정렬.py
@@ -1,7 +1,5 @@
 import re
-
 def solution(files):
     arr = [re.split(r"([0-9]+)", file) for file in files]
     arr.sort(key=lambda x: (x[0].lower(), int(x[1])))
-
     return [''.join(a) for a in arr]
