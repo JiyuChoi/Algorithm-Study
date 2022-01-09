fa2e787f90a31ff73d090ba4963d586c1ad686a3
JiyuChoi <jiyou126@naver.com>
2022-01-09 23:19:08 +0900

    Programmers 부족한 금액 계산하기

--- Programmers/부족한 금액 계산하기.py
+++ Programmers/부족한 금액 계산하기.py
@@ -3,3 +3,12 @@ def solution(price, money, count):
     total = money - sum([price*i for i in range(1, count+1)])
     answer = abs(total) if total < 0 else 0
     return answer
+
+
+def solution(price, money, count):
+    answer = -1
+    pay = 0
+    for i in range(count):
+        pay += (i+1)*price
+    answer = pay - money
+    return 0 if answer < 0 else answer
