2958b16ea2f90dc897fa5aa3580e6fb7d8de2c0e
JiyuChoi <jiyou126@naver.com>
2022-01-29 09:21:15 +0900

    BOJ 10451

--- BOJ/[DFS] 순열 사이클.py
+++ BOJ/[DFS] 순열 사이클.py
@@ -1,24 +1,54 @@
-# [텀 프로젝트]와 유사
+# # [텀 프로젝트]와 유사
+# import sys
+# sys.setrecursionlimit(1000000)
+#
+# def dfs(v):
+#     global cnt
+#     visited[v] = 1
+#     cycle.append(v)
+#     next = route[v]
+#     if visited[next]:
+#         if next in cycle:
+#             cnt += 1
+#     else:
+#         dfs(next)
+#
+# for _ in range(int(input())):
+#     n = int(input())
+#     route = [0] + list(map(int, input().split()))
+#     visited = [1] + [0] * n
+#     cnt = 0
+#     for i in range(1, n+1):
+#         if not visited[i]:
+#             cycle = []
+#             dfs(i)
+#
+#     print(cnt)
+
 import sys
 sys.setrecursionlimit(1000000)
 
+
 def dfs(v):
     global cnt
-    visited[v] = 1
     cycle.append(v)
-    next = route[v]
-    if visited[next]:
-        if next in cycle:
+    visited[v] = 1
+    nx = num[v]
+
+    if visited[nx]:
+        if nx in cycle:
             cnt += 1
     else:
-        dfs(next)
+        dfs(nx)
+
 
 for _ in range(int(input())):
     n = int(input())
-    route = [0] + list(map(int, input().split()))
-    visited = [1] + [0] * n
+    num = [0] + list(map(int, input().split()))
+    visited = [0] * (n + 1)
     cnt = 0
-    for i in range(1, n+1):
+
+    for i in range(1, n + 1):
         if not visited[i]:
             cycle = []
             dfs(i)
