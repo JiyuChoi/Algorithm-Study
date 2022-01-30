7c022dc92465677d5a1e3ecfba04126f7e49f54e
JiyuChoi <jiyou126@naver.com>
2022-01-30 18:18:08 +0900

    BOJ 17610

--- BOJ/[DFS] 양팔저울.py
+++ BOJ/[DFS] 양팔저울.py
@@ -1,14 +1,31 @@
+# def dfs(l, tot):
+#     global res
+#     if l == n:
+#         if 0 < tot <= sum(w):
+#             res.add(tot)
+#
+#     else:
+#         dfs(l+1, tot+w[l])
+#         dfs(l+1, tot-w[l])
+#         dfs(l+1, tot)
+#
+#
+# n = int(input())
+# w = list(map(int, input().split()))
+# res = set()
+# dfs(0, 0)
+#
+# print(sum(w)-len(res))
+
 def dfs(l, tot):
-    global res
     if l == n:
         if 0 < tot <= sum(w):
             res.add(tot)
-
+        return
     else:
-        dfs(l+1, tot+w[l])
-        dfs(l+1, tot-w[l])
-        dfs(l+1, tot)
-
+        dfs(l + 1, tot + w[l])
+        dfs(l + 1, tot - w[l])
+        dfs(l + 1, tot)
 
 n = int(input())
 w = list(map(int, input().split()))
