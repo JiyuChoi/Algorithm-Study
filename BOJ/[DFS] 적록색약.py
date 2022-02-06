4d787dcca127916af4016aed3ad81b8446e5cf0b
JiyuChoi <jiyou126@naver.com>
2022-02-06 19:38:37 +0900

    BOJ 10026번

--- BOJ/[DFS] 적록색약.py
+++ BOJ/[DFS] 적록색약.py
@@ -42,3 +42,50 @@ for _ in range(2):
     flag = 1
 
 print(*res)
+
+
+# BFS 풀이
+from collections import deque
+
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+
+n = int(input())
+board = [list(input()) for _ in range(n)]
+
+visited = [[0]*n for _ in range(n)]
+cnt_3 = 0
+
+def bfs(x, y):
+    visited[x][y] = 1
+    q = deque([(x, y)])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and board[nx][ny] == board[x][y]:
+                q.append((nx, ny))
+                visited[nx][ny] = 1
+
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            cnt_3 += 1
+            bfs(i, j)
+
+cnt_2 = 0
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 'R':
+            board[i][j] = 'G'
+
+visited = [[0]*n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            cnt_2 += 1
+            bfs(i, j)
+
+print(cnt_3, cnt_2)
