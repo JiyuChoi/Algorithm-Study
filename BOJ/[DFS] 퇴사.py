21e77983d636dc7976b13e39f8d1be54235eb965
JiyuChoi <jiyou126@naver.com>
2022-02-09 10:06:18 +0900

    BOJ 14501

--- BOJ/[DFS] 퇴사.py
+++ BOJ/[DFS] 퇴사.py
@@ -1,30 +1,54 @@
-def dfs(l, price):
+# def dfs(l, price):
+#     global max_v
+#     # 날짜가 n을 넘겼다면 종료
+#     if l > n:
+#         return
+#     # 날짜가 n이라면 최댓값 비교
+#     if l == n:
+#         if price > max_v:
+#             max_v = price
+#
+#     else:
+#         # l일에 상담을 하는 경우
+#         dfs(l+T[l], price+P[l])
+#         # l일에 상담을 하지 않는 경우
+#         dfs(l+1, price)
+#
+# # n값 입력
+# n = int(input())
+# T = []
+# P = []
+# # t, p 각각 T, P 리스트에 입력
+# for _ in range(n):
+#     t, p = map(int, input().split())
+#     T.append(t)
+#     P.append(p)
+#
+# max_v = float('-inf')
+#
+# dfs(0, 0)
+#
+# print(max_v)
+
+def dfs(l, p):
     global max_v
-    # 날짜가 n을 넘겼다면 종료
     if l > n:
         return
-    # 날짜가 n이라면 최댓값 비교
     if l == n:
-        if price > max_v:
-            max_v = price
-
+        max_v = max(max_v, p)
     else:
-        # l일에 상담을 하는 경우
-        dfs(l+T[l], price+P[l])
-        # l일에 상담을 하지 않는 경우
-        dfs(l+1, price)
+        dfs(l+time[l], p+pay[l])
+        dfs(l+1, p)
 
-# n값 입력
 n = int(input())
-T = []
-P = []
-# t, p 각각 T, P 리스트에 입력
+time = []
+pay = []
 for _ in range(n):
     t, p = map(int, input().split())
-    T.append(t)
-    P.append(p)
+    time.append(t)
+    pay.append(p)
 
-max_v = float('-inf')
+max_v = float("-inf")
 
 dfs(0, 0)
 
