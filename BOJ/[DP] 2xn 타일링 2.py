4ee669f77e7b86e683da86ea094505f20d723bb7
JiyuChoi <jiyou126@naver.com>
2022-03-21 18:26:10 +0900

    BOJ 11727 (S3)

--- BOJ/[DP] 2xn 타일링 2.py
+++ BOJ/[DP] 2xn 타일링 2.py
@@ -1,5 +1,15 @@
+# n = int(input())
+# dy = [0, 1, 3]
+# for i in range(3, n + 1):
+#     dy.append((dy[i-1] + 2*dy[i-2])%10007)
+# print(dy[n])
+
 n = int(input())
-dy = [0, 1, 3]
-for i in range(3, n + 1):
-    dy.append((dy[i-1] + 2*dy[i-2])%10007)
-print(dy[n])
+dy = [0]*1001
+dy[1] = 1
+dy[2] = 3
+
+for i in range(3, n+1):
+    dy[i] = dy[i-1] + dy[i-2]*2
+
+print(dy[n]%10007)
