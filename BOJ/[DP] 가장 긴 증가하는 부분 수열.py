8ed39f08ed8b75a2bc497e8fe6577785b203e3c7
JiyuChoi <jiyou126@naver.com>
2022-03-23 10:58:14 +0900

    BOJ 11053 (S2)

--- BOJ/[DP] 가장 긴 증가하는 부분 수열.py
+++ BOJ/[DP] 가장 긴 증가하는 부분 수열.py
@@ -1,11 +1,22 @@
+# n = int(input())
+# num = list(map(int, input().split()))
+#
+# dy = [1]*n
+#
+# for i in range(1, n):
+#     for j in range(i):
+#         if num[i] > num[j]:
+#             dy[i] = max(dy[i], dy[j]+1)
+#
+# print(max(dy))
+
 n = int(input())
 num = list(map(int, input().split()))
-
-dy = [1]*n
+dp = [1]*n
 
 for i in range(1, n):
     for j in range(i):
         if num[i] > num[j]:
-            dy[i] = max(dy[i], dy[j]+1)
+            dp[i] = max(dp[i], dp[j]+1)
 
-print(max(dy))
+print(max(dp))
