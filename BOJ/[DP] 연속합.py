286a63f552575d7835442410657f292c0cef9181
JiyuChoi <jiyou126@naver.com>
2022-03-23 16:47:52 +0900

    BOJ 1912 (S2)

--- BOJ/[DP] 연속합.py
+++ BOJ/[DP] 연속합.py
@@ -1,10 +1,17 @@
 n = int(input())
 num = list(map(int, input().split()))
+
 dp = [0]*n
+# for i in range(n):
+#     dp[i] = max(dp[i-1] + num[i], num[i]) #그 전까지 (합한 값+ 더할 값)과 현재 더할 값을 비교해 큰 값을 넣기
+#     # if dp[i-1] + num[i] > num[i]:
+#     #     dp[i] = dp[i-1] + num[i]
+#     # else:
+#     #     dp[i] = num[i]
+# print(max(dp))
+
+
 for i in range(n):
-    dp[i] = max(dp[i-1] + num[i], num[i]) #그 전까지 (합한 값+ 더할 값)과 현재 더할 값을 비교해 큰 값을 넣기
-    # if dp[i-1] + num[i] > num[i]:
-    #     dp[i] = dp[i-1] + num[i]
-    # else:
-    #     dp[i] = num[i]
+    dp[i] = max(dp[i-1]+num[i], dp[i]+num[i])
+
 print(max(dp))
