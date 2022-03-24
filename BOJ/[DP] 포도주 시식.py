6abe01b92a0e5f7c5ff584cddfa769f171e0757a
JiyuChoi <jiyou126@naver.com>
2022-03-24 23:26:25 +0900

    BOJ 2156 (S1)

--- BOJ/[DP] 포도주 시식.py
+++ BOJ/[DP] 포도주 시식.py
@@ -1,13 +1,26 @@
 # 계단 오르기와 다른 점은 꼭 마시지 않아도 되므로 두 번 이상 마시지 않을 수도 있다!
 
+# n = int(input())
+# wine = [0] + [int(input()) for _ in range(n)]
+#
+# dp = [0]*(n+1)
+# dp[1] = wine[1]
+#
+# if n >= 2:
+#     dp[2] = max(wine[1] + wine[2], dp[0] + wine[2])
+#     for i in range(3, n+1):
+#         dp[i] = max(dp[i-2]+wine[i], dp[i-3]+wine[i-1]+wine[i], dp[i-1])
+# print(dp[-1])
+
 n = int(input())
-wine = [0] + [int(input()) for _ in range(n)]
+grape = [0] + [int(input()) for _ in range(n)]
 
 dp = [0]*(n+1)
-dp[1] = wine[1]
+dp[1] = grape[1]
 
 if n >= 2:
-    dp[2] = max(wine[1] + wine[2], dp[0] + wine[2])
+    dp[2] = max(grape[2]+dp[0], grape[2]+grape[1], dp[1])
     for i in range(3, n+1):
-        dp[i] = max(dp[i-2]+wine[i], dp[i-3]+wine[i-1]+wine[i], dp[i-1])
+        dp[i] = max(dp[i-1], dp[i-2]+grape[i], dp[i-3]+grape[i-1]+grape[i])
+
 print(dp[-1])
