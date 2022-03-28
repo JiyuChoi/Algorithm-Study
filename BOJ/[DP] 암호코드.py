28e05fb1d2e4023a0a567fc0559b2495763c639d
JiyuChoi <jiyou126@naver.com>
2022-03-28 17:14:26 +0900

    BOJ 2011 (G5)

---
+++ BOJ/[DP] 암호코드.py
@@ -0,0 +1,18 @@
+n = [0] + list(map(int,input()))
+
+l = len(n) - 1
+dp = [0]*(l+1)
+dp[0], dp[1] = 1, 1
+
+if n[1] == 0: # 맨 앞 숫자가 0이면 만들 수 없음!
+    print("0")
+else:
+    for i in range(2, l+1):
+        if n[i] > 0:
+            dp[i] += dp[i-1]
+
+        temp = n[i-1] * 10 + n[i]
+        if 10 <= temp <= 26:
+            dp[i] += dp[i-2]
+
+    print(dp[l] % 1000000)
