a45a12f65e74c3f0d203d6db69c70949d004878d
JiyuChoi <jiyou126@naver.com>
2022-03-29 16:09:52 +0900

    BOJ 11052 (S1)

--- BOJ/[DP] 카드 구매하기.py
+++ BOJ/[DP] 카드 구매하기.py
@@ -14,6 +14,6 @@ dp = [0]*(n+1)
 
 for i in range(1, n+1):
     for j in range(1, i+1):
-        dp[i] = max(dp[i], dp[i-j] + cost[j])
+        dp[i] = max(dp[i], dp[i-j] + cost[j]) # for j 돌아가는 값에서 비교!
 
 print(dp[-1])
